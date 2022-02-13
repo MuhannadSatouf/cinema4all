59e28b8e8fde8b70ef0031fd446d2d5da73e0c6c
MuhannadSatouf <muhannad.satouf0016@stud.hkr.se>
2022-02-13 11:47:53 +0100

    Filter buttom, edit filtering

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 //serve all the files in the frontend folder
 app.use(express.static("frontend"));
 //start the webserver on port 3000
-app.listen(3000, () => console.log("//http:localhost:3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("//http:localhost:3000")
+);
 
-const api = require('./rest-api');
+const api = require("./rest-api");
 api(app);
