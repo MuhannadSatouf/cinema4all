c78a140a59f86f87e2864d11340285e1c0f3d3cb
liliia <liliyameister@gmail.com>
2022-02-21 14:23:19 +0100

    added a get request for selecting booking headers and lines

--- rest-api.js
+++ rest-api.js
@@ -38,8 +38,10 @@ module.exports = function api(app) {
     });
   }
 
-  //Create a route to get a single post by its id
-  for (let { name } of together) {
+  //Create a route to get a single post by its id (except validateUser): 
+  // this will help the logIn function validate the user via link http://localhost:3000/api/validateUser/{lee}
+  //You are supposed to retrieve the user's Id and then use another view (api/allCustomers/id) to get all the user-related info
+  for (let { name } of views) {
     if ({ name }.name === 'validateUser') {
       app.get('/api/validateUser/' + ':userAlias', (req, res) => {
         let stmt = db.prepare(`
@@ -64,9 +66,35 @@ module.exports = function api(app) {
     }
   }
 
-  // this will help the logIn function validate the user. 
-  //You are supposed to retrieve the user's Id and then use another view (api/allCustomers/id) to get all the user-related info
 
-
+  //this lets filter booking headers by userId and schedule Id. One recent record till be returned. 
+  //Made for getting the header id to then use it for booking lines creation.
+  for (let { name } of tables) {
+    if ({ name }.name === 'bookingHeader') {
+      app.get('/api/' + name + '/' + ':userId' + '/' + ':scheduleId', (req, res) => {
+        let stmt = db.prepare(`
+        select * from ${name}
+          where 
+          userId = :userId and
+          scheduleId = :scheduleId
+          order by id desc limit 1;
+    `);
+        let result = stmt.all(req.params)[0] || null;
+        if (result === null) { res.status(404); }
+        res.json(result);
+      });
+      // booking lines will be returned for a specific header Id.
+    } else if ({ name }.name === 'bookingLine') {
+      app.get('/api/' + name + '/:bookingId', (req, res) => {
+        let stmt = db.prepare(`
+        SELECT * FROM ${name} 
+        where bookingId = :bookingId 
+    `);
+        let result = stmt.all(req.params) || null;
+        if (result === null) { res.status(404); }
+        res.json(result);
+      });
+    }
+  }
 
 }
