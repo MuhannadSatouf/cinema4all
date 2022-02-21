12fe5ad204576c3798c46fc30daf0202ed16057d
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2022-02-21 14:14:47 +0100

    SPA and Login
    Made the app SPA and added a login page, functionallity for login will be added later.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,55 +5,7 @@ async function getData(restRoute) {
   return result;
 }
 
-let menu = document.querySelector("#fa-bars");
-let navbar = document.querySelector(".navbar");
 
-menu.onclick = () => {
-  menu.classList.toggle("fa-bars");
-  navbar.classList.toggle("active");
-};
-
-window.onscroll = () => {
-  menu.classList.remove("fa-bars");
-  navbar.classList.remove("active");
-};
-
-var swiper = new Swiper(".home", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 6000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-var swiper = new Swiper(".coming-container", {
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  centeredSlides: true,
-  breakpoints: {
-    0: {
-      slidesPerView: 2,
-    },
-    568: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-    968: {
-      slidesPerView: 5,
-    },
-  },
-});
 
 function renderMovieList(cssSelector, list) {
   let html = "";
@@ -108,7 +60,9 @@ function renderComingMovieList(cssSelector, list) {
   return html;
 }
 
-async function start() {
+ async function start() {
+
+  console.log("In Start")
   renderMovieList(
     ".active-movies-container",
     await getData("/api/allActiveMovies")
@@ -117,61 +71,53 @@ async function start() {
     ".coming-movies-container",
     await getData("/api/allComingMovies")
   );
-}
+let menu = document.querySelector("#fa-bars");
+let navbar = document.querySelector(".navbar");
 
-start();
+  menu.onclick = () => {
+  menu.classList.toggle("fa-bars");
+  navbar.classList.toggle("active");
+};
 
-/*
-document.querySelector("btn").addEventListener("click", function (event) {
-  let pageTag = event.target.closest("a");
+window.onscroll = () => {
+  menu.classList.remove("fa-bars");
+  navbar.classList.remove("active");
+};
 
-  if (!pageTag) {
-    return;
-  }
-
-  let href = pageTag.getAttribute("href");
-
-  if (href.IndexOf("http") ?? 0) {
-    pageTag.setAttribute("target", "_blank");
-
-    return;
-  }
-
-  event.preventDefault();
-
-  history.pushState(null, null, href);
-
-  router();
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-*/
 
-function makeMenuChoiceActive(prompt) {
-  // change active link in the menu
-  let aTagsInButton = document.querySelectorAll("a");
-  for (let aTag of aTagsInButton) {
-    aTag.classList.remove("active");
-    let href = aTag.getAttribute("href");
-    if (href === prompt) {
-      aTag.classList.add("active");
-    }
-  }
+var swiper = new Swiper(".coming-container", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    568: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    968: {
+      slidesPerView: 5,
+    },
+  },
+});
 }
-
-async function router() {
-  let prompt = location.pathname;
-
-  makeMenuChoiceActive(prompt);
-
-  prompt = prompt === "/" ? "./frontend/" : prompt;
-  prompt = "/partials" + prompt + ".html";
-
-  let content = await (await fetch(prompt)).text();
-
-  content.includes("<title>Error: Could not find page</title>");
-
-  document.querySelector("main").innerHTML = content;
-
-  prompt === "./partials/login.html";
-}
-
-window.addEventListener("popstate", router);
