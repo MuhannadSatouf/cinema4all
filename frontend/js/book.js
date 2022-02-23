7e4aff033153875f8a2673f3ab388074ee7e7111
liliia <liliyameister@gmail.com>
2022-02-23 16:46:20 +0100

    booking options finally work

--- frontend/js/book.js
+++ frontend/js/book.js
@@ -1,107 +1,117 @@
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-let movieSelect = '1';
+//const container = document.querySelector('.container');
+//const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 
-renderOptionList().then(movieSelect = document.getElementById('movie'));
-populateUI();
 
-let ticketPrice = +movieSelect.value;
 
-// Save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
-}
+async function bookAMovie() {
+  let movieSelect = await renderOptionList();
+  let container = document.querySelector('.container');
+  let seats = document.querySelectorAll('.row .seat:not(.occupied)');
+  let count = document.getElementById('count');
+  let total = document.getElementById('total');
+  //console.log(movieSelect);
 
-// Update total and count
-function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  populateUI();
 
-  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  let ticketPrice = +movieSelect.value;
 
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-
-  const selectedSeatsCount = selectedSeats.length;
-
-  count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
-
-  setMovieData(movieSelect.selectedIndex, movieSelect.value);
-}
-
-// Get data from localstorage and populate UI
-function populateUI() {
-  // renderOptionList();
-
-  movieSelect = document.getElementById('movie');
-
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    seats.forEach((seat, index) => {
-      if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add('selected');
-      }
-    });
+  // Save selected movie index and price
+  function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
   }
 
-  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  // Update total and count
+  function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-  if (selectedMovieIndex !== null) {
-    movieSelect.selectedIndex = selectedMovieIndex;
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
   }
-}
 
-// Movie select event
-movieSelect.addEventListener('change', e => {
-  ticketPrice = +e.target.value;
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-});
+  // Get data from localstorage and populate UI
+  function populateUI() {
+    // renderOptionList();
 
-// Seat click event
-container.addEventListener('click', e => {
-  if (
-    e.target.classList.contains('seat') &&
-    !e.target.classList.contains('occupied')
-  ) {
-    e.target.classList.toggle('selected');
+    movieSelect = document.getElementById('movie');
 
-    updateSelectedCount();
-  }
-});
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
-// Initial count and total set
-updateSelectedCount();
-
-async function renderOptionList() {
-  let list = await getData("/api/scheduleFilter");
-  let html = "";
-  html += '<label>Make your choice:</label>';
-  html += '<select id="movie">';
-  let i = 0;
-  console.log('list length ' + list.length);
-  for (let option of list) {
-    if (i === 0) {
-      html += '<option selected value="';
-    } else {
-      html += '<option value="';
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected');
+        }
+      });
     }
-    html += option.scheduleId + '">';
-    html += option.title + ' ' + option.date + ' ' + option.time;
-    html += '</option>';
-    i++;
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+    }
   }
-  html += '</select>';
 
-  console.log(html);
-  i = 0;
+  // Movie select event
+  movieSelect.addEventListener('change', e => {
+    // ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    //updateSelectedCount();
+    console.log(e.target.innerText);
+  });
 
-  // document.querySelector('#movie').innerHTML = html;
+  // Seat click event
+  container.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('seat') &&
+      !e.target.classList.contains('occupied')
+    ) {
+      e.target.classList.toggle('selected');
 
-  document.querySelector(".movie-container").innerHTML = html;
+      updateSelectedCount();
+    }
+  });
 
-  return html;
+  // Initial count and total set
+  updateSelectedCount();
+
+  async function renderOptionList() {
+    let list = await getData("/api/scheduleFilter");
+    let html = "";
+    html += '<label>Make your choice:</label>';
+    html += '<select name = "drop" id="movie">';
+    let i = 0;
+
+    for (let option of list) {
+      if (i === 0) {
+        html += '<option selected value="';
+      } else {
+        html += '<option value="';
+      }
+      html += option.scheduleId + '">';
+      html += option.title + ' ' + option.date + ' ' + option.time;
+      html += '</option>';
+      i++;
+    }
+    html += '</select>';
+
+    i = 0;
+
+    document.querySelector('.movie-container').innerHTML = html;
+
+    return document.querySelector('.movie-container').querySelectorAll('option');
+  }
+
+
 }
+//let movieSelect;
+
+bookAMovie();
